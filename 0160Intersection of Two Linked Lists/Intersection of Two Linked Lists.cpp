fdbf2e6937ea25c3553769a9a65515e23ef55623
CHANG, HSU-JUNG <71311968+tcfshDoer@users.noreply.github.com>
2026-01-13 12:25:33 +0800

    Create Intersection of Two Linked Lists.cpp

---
+++ 0160Intersection of Two Linked Lists/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {//當鍊表長部不同時，嘗試找一種遍歷方法能夠同時遍歷到相交的節點
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+ListNode* tempA = headA;
+ListNode* tempB = headB;
+        
+    }
+};
